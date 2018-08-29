7fd3fdc3890b4cc81eb15e58c6eef832abd6733b
Leonardo_zyh <956731563@qq.com>
2018-08-29 09:03:45 +0800

    更新,再次build

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 这是我的第一个React应用
+#Create React App创建
+npm install -g create-react-app
+create-react-app my-app
+cd my-app
+npm start
+
+#分析代码
+import './index.css'   (引入css) 
+import App from './App.js'  (引入js)
+ReactDOM.render(<App />,div );  (render,标签渲染且放置在div)
+
+App标签
+简写XML,通过JSX转换为js
+onClick={function(){alert('hi')}}  花括号内为js，括号外为XML
+
+#代码实时更新（npm start）
+
+#展示页面
+npm run-script build
+package中加  "homepage" : "https://leonardo-zyh.github.io/react-learn/build",
+
+.gitignore中production去除build，使得能够git add
--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React不过如此</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={()=>{alert('hi')}} >点我</button>
+        <button onClick={()=>{alert('Hi,this is my react test')}} >点我</button>
       </div>//XML,通过JSX转换为js
     );
   }
